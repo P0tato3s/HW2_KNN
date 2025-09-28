43f35780aecf11b9d1d3b7da3bbcca6fd0c9f1b4
John <lijohn2004@gmail.com>
2025-09-28 15:35:34 -0700

    test run

--- knn.py
+++ knn.py
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: John Li
+# FILENAME: knn.py
+# SPECIFICATION: Using KNN to classify emails as spam or not spam
 # FOR: CS 4210- Assignment #2
 # TIME SPENT: how long it took you to complete the assignment
 #-----------------------------------------------------------*/
@@ -10,6 +10,7 @@
 
 #Importing some Python libraries
 from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
 import pandas as pd
 
 #Reading the data in a csv file using pandas
@@ -24,30 +25,33 @@ for i in db:
     #Add the training features to the 20D array X removing the instance that will be used for testing in this iteration.
     #For instance, X = [[1, 2, 3, 4, 5, ..., 20]].
     #Convert each feature value to float to avoid warning messages
-    #--> add your Python code here
+    X = df.iloc[:, :-1].astype(float).to_numpy()
 
     #Transform the original training classes to numbers and add them to the vector Y.
     #Do not forget to remove the instance that will be used for testing in this iteration.
     #For instance, Y = [1, 2, ,...].
     #Convert each feature value to float to avoid warning messages
-    #--> add your Python code here
-
-    #Store the test sample of this iteration in the vector testSample
-    #--> add your Python code here
+    Y = df.iloc[:, -1].to_numpy()
 
     #Fitting the knn to the data using k = 1 and Euclidean distance (L2 norm)
     #--> add your Python code here
-    clf =
+    clf = KNeighborsClassifier(n_neighbors=2, metric='euclidean')
+    clf.fit(X, Y)
 
-    #Use your test sample in this iteration to make the class prediction. For instance:
-    #class_predicted = clf.predict([[1, 2, 3, 4, 5, ..., 20]])[0]
-    #--> add your Python code here
+    nn_idx = clf.kneighbors(X, return_distance=False)
+
+    loo_idx = nn_idx[:, 1]
+    Y_pred = Y[loo_idx]
 
     #Compare the prediction with the true label of the test instance to start calculating the error rate.
-    #--> add your Python code here
+    wrong = int(np.sum(Y_pred != Y))
+    N = len(Y)
+    error_rate = wrong / N
 
 #Print the error rate
-#--> add your Python code here
+print(f"N = {N}")
+print(f"Wrong predictions = {wrong}")
+print(f"LOO-CV error rate for 1NN = {error_rate:.4f}")
 
 
 
